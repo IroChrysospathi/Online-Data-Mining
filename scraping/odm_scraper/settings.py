26bb647aa5d8e9ee4883103ed356e6e5e01bf3f5
IroChrysospathi <iro.chrisospathi@gmail.com>
2026-01-15 22:46:44 +0100

    settings.py comments

--- scraping/odm_scraper/settings.py
+++ scraping/odm_scraper/settings.py
@@ -1,1 +1,9 @@
+"""
+Global Scrapy settings.
+
+Responsibilities:
+- Configure crawler behavior (delays, concurrency, robots.txt)
+- Enable or disable pipelines and middlewares
+- Control overall scraping configuration
+"""
 
