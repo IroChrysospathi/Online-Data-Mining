6ac58ca71d0ead6c1f1de48039f949661b4e8962
IroChrysospathi <iro.chrisospathi@gmail.com>
2026-01-15 22:55:05 +0100

    bol support reqs

--- scraping/odm_scraper/spiders/bol_support.py
+++ scraping/odm_scraper/spiders/bol_support.py
@@ -1,1 +1,9 @@
+"""
+Support spider.
+
+Responsibilities:
+- Crawl non-product pages (e.g., FAQ, warranty, support information)
+- Extract relevant informational content
+- Yield structured support data if required by the project
+"""
 
