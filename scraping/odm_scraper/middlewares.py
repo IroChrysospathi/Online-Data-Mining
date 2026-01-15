e9564e555aa82a97bdcaf7028fb1c89443f021c3
IroChrysospathi <iro.chrisospathi@gmail.com>
2026-01-15 22:48:02 +0100

    middleware.py reqs

--- scraping/odm_scraper/middlewares.py
+++ scraping/odm_scraper/middlewares.py
@@ -1,1 +1,9 @@
+"""
+Scrapy middlewares.
+
+Responsibilities:
+- Optionally modify requests and responses
+- Handle headers, retries, or custom crawling behavior
+- This file may remain unused if default Scrapy behavior is sufficient
+"""
 
