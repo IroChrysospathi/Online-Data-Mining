84cfd7b43c9bcd69e44774612f6762b26e3d4027
IroChrysospathi <iro.chrisospathi@gmail.com>
2026-01-22 22:01:55 +0100

    Delete scraping/odm_scraper/spiders/maxiaxi_support.py

--- scraping/odm_scraper/spiders/maxiaxi_support.py
+++
@@ -1,9 +0,0 @@
-"""
-Support spider.
-
-Responsibilities:
-- Crawl non-product pages (e.g., FAQ, warranty, support information)
-- Extract relevant informational content
-- Yield structured support data if required by the project
-"""
-
